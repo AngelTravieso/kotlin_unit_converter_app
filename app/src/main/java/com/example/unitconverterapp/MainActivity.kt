d1f8cb383f5aa66f787e30c8452265f25e801c8a
AngelTravieso <angeltraviesoc@gmail.com>
2023-11-21 10:08:46 -0400

    Declaring widgets

--- app/src/main/java/com/example/unitconverterapp/MainActivity.kt
+++ app/src/main/java/com/example/unitconverterapp/MainActivity.kt
@@ -2,13 +2,18 @@ package com.example.unitconverterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        val edt = findViewById<EditText>(R.id.kilo_edt)
+        val btn = findViewById<Button>(R.id.button)
+        val resultText = findViewById<TextView>(R.id.result_text)
 
     }
 }
